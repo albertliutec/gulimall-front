04ff1391e4dd88f0db544958897f2bd70517b742
liu <602389974@qq.com>
2022-04-18 17:53:02 +0800

    edit能力完成2022041801

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -21,6 +21,9 @@
           >
             Append
           </el-button>
+          <el-button type="text" size="mini" @click="() => edit(data)">
+            edit
+          </el-button>
           <el-button
             v-if="node.childNodes.length == 0"
             type="text"
@@ -33,20 +36,35 @@
       </span>
     </el-tree>
 
-    <!-- 对话框 -->
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <!-- 添加，修改对话框 -->
+    <el-dialog
+      :close-on-click-modal="false"
+      :title="dialogTitle"
+      :visible.sync="dialogVisible"
+      width="30%"
+    >
       <el-form :model="category">
         <el-form-item label="分类名称">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input
+            v-model="category.productUnit"
+            autocomplete="off"
+          ></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button @click="cleanDialog">取 消</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
+
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
@@ -58,6 +76,7 @@ export default {
 
   data() {
     return {
+      // 菜单属性
       // 菜单数组
       menus: [],
       // 需要点击后展开的菜单
@@ -67,15 +86,25 @@ export default {
         children: "children",
         label: "name",
       },
+
+      // 对话框属性
+      // 对话框tittle
+      dialogTitle: null,
       // 对话框是否可见
       dialogVisible: false,
+      // 对话框类型
+      dialogType: null,
+
       // 对话框表单绑定对象
       category: {
-        name: "",
-        parentCid: 0,
-        catLevel: 0,
+        catId: null,
+        name: null,
+        parentCid: null,
+        catLevel: null,
         showStatus: 1,
         sort: 0,
+        icon: null,
+        productUnit: null,
       },
     };
   },
@@ -87,18 +116,107 @@ export default {
         method: "get",
       }).then(({ data }) => {
         this.menus = data.entityList;
-        // console.log(this.menus)
       });
     },
 
-    append(data) {
+    // 打开添加对话框
+    async append(data) {
+      this.dialogTitle = "添加分类";
+      this.dialogType = "append";
+      await this.getBaseInfo(data);
+      // 获取上级菜单ID
+      this.category.parentCid = this.category.catId;
+      this.category.catLevel = this.category.catLevel * 1 + 1;
+      console.log(this.category);
+
+      // 更新种类信息为待填写状态
+      this.category.catId = null;
+      this.category.name = null;
+      this.category.showStatus = 1;
+      this.category.sort = 0;
+      this.category.icon = null;
+      this.category.productUnit = null;
+      // console.log(this.category);
+
       // 显示添加对话框
       this.dialogVisible = true;
-      // 配置基本属性
-      this.category.parentCid = data.catId;
-      this.category.catLevel = data.catLevel * 1 + 1;
     },
 
+    // 打开修改对话框
+    async edit(data) {
+      this.dialogTitle = "修改分类";
+      this.dialogType = "edit";
+      await this.getBaseInfo(data);
+      // console.log(this.category);
+      // 显示添加对话框
+      this.dialogVisible = true;
+    },
+
+    // 提交数据
+    async submitData() {
+      if (this.dialogType == "append") {
+        await this.save("save", "添加");
+      }
+      if (this.dialogType == "edit") {
+        await this.save("update", "修改");
+      }
+    },
+
+    // 获取某种类全量信息
+    async getBaseInfo(data) {
+      await this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get",
+      }).then(({ data }) => {
+        // 配置基本属性
+        this.category = data.category;
+      });
+    },
+
+    // 清空并关闭对话框
+    cleanDialog() {
+      // 属性清空
+      this.category.catId = null;
+      this.category.name = null;
+      this.category.parentCid = null;
+      this.category.catLevel = null;
+      this.category.showStatus = 1;
+      this.category.sort = 0;
+      this.category.icon = null;
+      this.category.productUnit = null;
+      // 对话框清空
+      this.dialogTitle = null;
+      this.dialogVisible = false;
+    },
+
+    // 添加保存数据库
+    async save(url, action) {
+      // 发请求
+      await this.$http({
+        url: this.$http.adornUrl(`/product/category/${url}`),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      })
+        .then((data) => {
+          // 更新
+          this.getMenus();
+          //展开父级菜单
+          this.expandedKey = [this.category.parentCid];
+          // 提示成功
+          this.$message({
+            type: "success",
+            message: `${action}成功！`,
+          });
+        })
+        .catch(() => {
+          // 提示失败
+          this.$message.error(`${action}失败！`);
+        });
+      // 清空并关闭对话框
+      this.cleanDialog();
+    },
+
+    // 删除
     remove(node, data) {
       this.$confirm(`是否删除【${data.name}】菜单`, "提示", {
         confirmButtonText: "确定",
@@ -127,32 +245,6 @@ export default {
             this.$message.error("删除失败！");
           });
       });
-    },
-
-    addCategory() {
-      // 发请求
-      this.$http({
-        url: this.$http.adornUrl("/product/category/save"),
-        method: "post",
-        data: this.$http.adornData(this.category, false),
-      })
-        .then((returndata) => {
-          // 更新
-          this.getMenus();
-          //展开父级菜单
-          this.expandedKey = [this.category.parentCid];
-          // 提示成功
-          this.$message({
-            type: "success",
-            message: "添加成功！",
-          });
-        })
-        .catch(() => {
-          // 提示失败
-          this.$message.error("添加失败！");
-        });
-      // 关闭对话框
-      this.dialogVisible = false;
     },
   },
   //监听属性 类似于data概念
