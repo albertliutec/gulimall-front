14058e80481a3f53eb698d2bfe341522d512e7b7
liu <602389974@qq.com>
2022-04-24 02:11:01 +0800

    替换前端页面后第一次同步 2022042401

--- src/views/modules/common/category.vue
+++ src/views/modules/common/category.vue
@@ -1,75 +1,82 @@
 <template>
-  <!-- 分级菜单展示 -->
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    node-key="catId"
-    ref="menuTree"
-    @node-click="nodeClick"
-  >
-  </el-tree>
+  <div>
+    <el-input placeholder="输入关键字进行过滤" v-model="filterText"></el-input>
+    <el-tree
+      :data="menus"
+      :props="defaultProps"
+      node-key="catId"
+      ref="menuTree"
+      @node-click="nodeclick"
+      :filter-node-method="filterNode"
+      :highlight-current = "true"
+    ></el-tree>
+  </div>
 </template>
 
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
-//例如：import 《组件名称》from '《组件路径》';
+//例如：import 《组件名称》 from '《组件路径》';
 
 export default {
-  //import 引入的组件需要注入到对象中才能使用
+  //import引入的组件需要注入到对象中才能使用
   components: {},
   props: {},
   data() {
+    //这里存放数据
     return {
-      // 菜单变更属性
-      // 菜单数组
+      filterText: "",
       menus: [],
-      // 需要点击后展开的菜单
       expandedKey: [],
-      // menus中数据与el-tree属性对应关系
       defaultProps: {
         children: "children",
-        label: "name",
-      },
+        label: "name"
+      }
     };
   },
-  //计算属性类似于data 概念
+  //计算属性 类似于data概念
   computed: {},
-  //监控data 中的数据变化
-  watch: {},
+  //监控data中的数据变化
+  watch: {
+    filterText(val) {
+      this.$refs.menuTree.filter(val);
+    }
+  },
   //方法集合
   methods: {
-    // 刷新表单
+    //树节点过滤
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.name.indexOf(value) !== -1;
+    },
     getMenus() {
       this.$http({
         url: this.$http.adornUrl("/product/category/list/tree"),
-        method: "get",
+        method: "get"
       }).then(({ data }) => {
-        this.menus = data.entityList;
+        this.menus = data.data;
       });
     },
-
-    nodeClick(data, node, component) {
-      console.log("son:",data, node, component);
-
-      // 向父组件发送时间
+    nodeclick(data, node, component) {
+      console.log("子组件category的节点被点击", data, node, component);
+      //向父组件发送事件；
       this.$emit("tree-node-click", data, node, component);
-    },
+    }
   },
-  //生命周期- 创建完成（可以访问当前this 实例）
+  //生命周期 - 创建完成（可以访问当前this实例）
   created() {
     this.getMenus();
   },
-  //生命周期- 挂载完成（可以访问DOM 元素）
+  //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {},
-  beforeCreate() {}, //生命周期- 创建之前
-  beforeMount() {}, //生命周期- 挂载之前
-  beforeUpdate() {}, //生命周期- 更新之前
-  updated() {}, //生命周期- 更新之后
-  beforeDestroy() {}, //生命周期- 销毁之前
-  destroyed() {}, //生命周期- 销毁完成
-  activated() {}, //如果页面有keep-alive 缓存功能，这个函数会触发
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
-<style lang='scss' scoped>
-//@import url(); 引入公共css 类
+<style scoped>
+
 </style>
--- src/views/modules/product/attrgroup-add-or-update.vue
+++ src/views/modules/product/attrgroup-add-or-update.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog
-    :title="!dataForm.attrGroupId ? '新增' : '修改'"
+    :title="!dataForm.id ? '新增' : '修改'"
     :close-on-click-modal="false"
     :visible.sync="visible"
     @closed="dialogClose"
@@ -10,13 +10,10 @@
       :rules="dataRule"
       ref="dataForm"
       @keyup.enter.native="dataFormSubmit()"
-      label-width="80px"
+      label-width="120px"
     >
       <el-form-item label="组名" prop="attrGroupName">
-        <el-input
-          v-model="dataForm.attrGroupName"
-          placeholder="组名"
-        ></el-input>
+        <el-input v-model="dataForm.attrGroupName" placeholder="组名"></el-input>
       </el-form-item>
       <el-form-item label="排序" prop="sort">
         <el-input v-model="dataForm.sort" placeholder="排序"></el-input>
@@ -27,15 +24,11 @@
       <el-form-item label="组图标" prop="icon">
         <el-input v-model="dataForm.icon" placeholder="组图标"></el-input>
       </el-form-item>
-      <el-form-item label="所属分类id" prop="catelogId">
-        <!-- <el-input v-model="dataForm.catelogId" placeholder="所属分类id"></el-input> -->
-        <el-cascader
-          v-model="dataForm.catelogPath"
-          :options="categorys"
-          :props="props"
-          placeholder="搜索"
-          filterable
-        ></el-cascader>
+      <el-form-item label="所属分类" prop="catelogId">
+        <!-- <el-input v-model="dataForm.catelogId" placeholder="所属分类id"></el-input> @change="handleChange" -->
+        <!-- <el-cascader filterable placeholder="试试搜索：手机" v-model="catelogPath" :options="categorys"  :props="props"></el-cascader> -->
+        <!-- :catelogPath="catelogPath"自定义绑定的属性，可以给子组件传值 -->
+        <category-cascader :catelogPath.sync="catelogPath"></category-cascader>
       </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -46,75 +39,67 @@
 </template>
 
 <script>
+import CategoryCascader from '../common/category-cascader'
 export default {
   data() {
     return {
-      // 级联选择器显示
-      props: {
-        value: "catId",
-        label: "name",
-        children: "children",
+      props:{
+        value:"catId",
+        label:"name",
+        children:"children"
       },
       visible: false,
       categorys: [],
-
-      // 级联选择器修改或删除部分
+      catelogPath: [],
       dataForm: {
         attrGroupId: 0,
         attrGroupName: "",
         sort: "",
         descript: "",
         icon: "",
-        catelogId: "",
-        // 同时用于显示，新增，删除
-        catelogPath: [],
+        catelogId: 0
       },
-
-      // 前端数据校验
-      // 修改或删除校验
       dataRule: {
         attrGroupName: [
-          { required: true, message: "组名不能为空", trigger: "blur" },
+          { required: true, message: "组名不能为空", trigger: "blur" }
         ],
         sort: [{ required: true, message: "排序不能为空", trigger: "blur" }],
         descript: [
-          { required: true, message: "描述不能为空", trigger: "blur" },
+          { required: true, message: "描述不能为空", trigger: "blur" }
         ],
         icon: [{ required: true, message: "组图标不能为空", trigger: "blur" }],
-        // catelogId: [
-        //   { required: true, message: "所属分类id不能为空", trigger: "blur" },
-        // ],
-        // 显示校验
-        catelogPath: [
-          { required: true, message: "所属分类id不能为空", trigger: "blur" },
-        ],
-      },
+        catelogId: [
+          { required: true, message: "所属分类id不能为空", trigger: "blur" }
+        ]
+      }
     };
   },
+  components:{CategoryCascader},
+  
   methods: {
-    // 获取category菜单
-    getCategorys() {
+    dialogClose(){
+      this.catelogPath = [];
+    },
+    getCategorys(){
       this.$http({
         url: this.$http.adornUrl("/product/category/list/tree"),
-        method: "get",
+        method: "get"
       }).then(({ data }) => {
-        this.categorys = data.entityList;
+        this.categorys = data.data;
       });
     },
-
     init(id) {
       this.dataForm.attrGroupId = id || 0;
       this.visible = true;
       this.$nextTick(() => {
         this.$refs["dataForm"].resetFields();
-        // 当修改或者删除的时候，id不是0才会调用此方法
         if (this.dataForm.attrGroupId) {
           this.$http({
             url: this.$http.adornUrl(
               `/product/attrgroup/info/${this.dataForm.attrGroupId}`
             ),
             method: "get",
-            params: this.$http.adornParams(),
+            params: this.$http.adornParams()
           }).then(({ data }) => {
             if (data && data.code === 0) {
               this.dataForm.attrGroupName = data.attrGroup.attrGroupName;
@@ -122,8 +107,8 @@ export default {
               this.dataForm.descript = data.attrGroup.descript;
               this.dataForm.icon = data.attrGroup.icon;
               this.dataForm.catelogId = data.attrGroup.catelogId;
-              // 查出catId的完整路径
-              this.dataForm.catelogPath = data.attrGroup.catelogPath;
+              //查出catelogId的完整路径
+              this.catelogPath =  data.attrGroup.catelogPath;
             }
           });
         }
@@ -131,7 +116,7 @@ export default {
     },
     // 表单提交
     dataFormSubmit() {
-      this.$refs["dataForm"].validate((valid) => {
+      this.$refs["dataForm"].validate(valid => {
         if (valid) {
           this.$http({
             url: this.$http.adornUrl(
@@ -146,10 +131,8 @@ export default {
               sort: this.dataForm.sort,
               descript: this.dataForm.descript,
               icon: this.dataForm.icon,
-              // 取最后一级菜单才是三级分类菜单
-              catelogId:
-                this.dataForm.catelogPath[this.dataForm.catelogPath.length - 1],
-            }),
+              catelogId: this.catelogPath[this.catelogPath.length-1]
+            })
           }).then(({ data }) => {
             if (data && data.code === 0) {
               this.$message({
@@ -158,9 +141,8 @@ export default {
                 duration: 1500,
                 onClose: () => {
                   this.visible = false;
-                  // 通知父组件更新分类表
                   this.$emit("refreshDataList");
-                },
+                }
               });
             } else {
               this.$message.error(data.msg);
@@ -168,16 +150,10 @@ export default {
           });
         }
       });
-    },
-
-    // 取消提交
-    dialogClose() {
-      this.dataForm.catelogPath = [];
-    },
+    }
   },
-
-  created() {
+  created(){
     this.getCategorys();
-  },
+  }
 };
 </script>
